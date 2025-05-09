17d1b5d3ded1a59b06ea2041954fa22cb740e42d
eldk24 <165836989+eldk24@users.noreply.github.com>
2025-05-09 09:49:05 -0500

    cambios

--- prueba-main/crud-vendedores/config/db.js
+++ prueba-main/crud-vendedores/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "yamabiko.proxy.rlwy.net",
-  user: "root",
-  password: "nKgSPhSJYWsqwwxZtZSBnYCoUtqlSgox",
-  database: "railway",
+  host: "MYSQL1002.site4now.net",
+  user: "eldk24",
+  password: "8Mariokart8",
+  database: "ventas",
   port: 47307,
   waitForConnections: true,
   connectionLimit: 10,
